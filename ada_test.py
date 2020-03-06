30114d295505f251205e2c90a42775542dd9a2b9
Ada Toydemir <toydemir@usc.edu>
2020-03-05 21:12:55 -0800

    beginnings of webscraping

---
+++ ada_test.py
@@ -0,0 +1,8 @@
+import requests
+from lxml import html
+
+page = requests.get('https://www.sltrib.com/news/education/2020/03/04/after-byu-honor-code/')
+contents = page.content
+tree = html.fromstring(contents)
+articleText = tree.xpath('//div[@id="article-body"@class="article-body article-body-elements"]/text()')
+print(articleText)
